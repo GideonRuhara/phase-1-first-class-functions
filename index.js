e2db6514f1be4ba39d05c9d7b85f28bd24156c9f
Gideon Ruhara <ruharagideon@gmail.com>
2022-12-07 12:47:41 +0300

    completed

---
+++ index.js
@@ -0,0 +1,29 @@
+let drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+const returnFirstTwoDrivers = function (){
+    let firstTwoDrivers = [...drivers]
+    return (firstTwoDrivers.slice(0 , 2))
+}
+const returnLastTwoDrivers = function(){
+    let LastTwoDrivers = [...drivers]
+    return (LastTwoDrivers.slice(2,4))
+}
+const selectingDrivers =[returnFirstTwoDrivers, returnLastTwoDrivers]
+const createFareMultiplier = function (fareMultiplier) {
+    return function(value) {
+      return fareMultiplier * value;
+    }
+  }
+  const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+const selectDifferentDrivers = function( drivers, func){
+    return ( func (returnFirstTwoDrivers || returnLastTwoDrivers));
+}
+
+
+
+
+
+
+
+
+
